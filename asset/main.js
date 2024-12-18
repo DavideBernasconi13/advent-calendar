2300ec2f3a6cb6d1cd77e91d40d3f902139a73c2
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-12-18 17:04:23 +0100

    add icon to cell

--- asset/main.js
+++ asset/main.js
@@ -1,3 +1,14 @@
+// mappatura delle icone 
+const path = 'asset/images/icons/';
+const icons = {
+    0: {
+        url: "ico-neve.png",
+        alt: "ico-neve"
+    }
+}
+
+
+
 // prendi il table calendar
 const calendarTable = document.getElementById('calendar');
 console.log(calendarTable);
@@ -6,7 +17,15 @@ for (i = 0; i <= 24; i++) {
     let el = document.createElement('div');
     el.classList.add('cell-day');
     el.innerHTML = i + 1;
+
+    //controlla se esiste icona associata 
+    if (icons[i]) {
+        const img = document.createElement("img");
+        img.src = path + icons[url];
+        img.alt = `Icona per la casella ${i}`;
+        el.appendChild(img);
+    }
     calendarTable.appendChild(el);
-    console.log(el.outerHTML);
 }
+
 
