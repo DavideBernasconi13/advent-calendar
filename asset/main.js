2b03f7d07142eb9e47961c195708bacb3a020361
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-12-18 16:04:21 +0100

    generate cell-day

--- asset/main.js
+++ asset/main.js
@@ -1,8 +1,12 @@
 // prendi il table calendar
-const calendarTable = document.getElementById('.calendar'); 
+const calendarTable = document.getElementById('calendar');
+console.log(calendarTable);
+// genera l'element cell-day, add class e appendi alla table
+for (i = 0; i <= 24; i++) {
+    let el = document.createElement('div');
+    el.classList.add('cell-day');
+    el.innerHTML = i + 1;
+    calendarTable.appendChild(el);
+    console.log(el.outerHTML);
+}
 
-// genera l'element cell-day 
-
-// appendi l'elemento 
-
-// cicla e stampa il numero
